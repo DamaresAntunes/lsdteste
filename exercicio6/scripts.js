38faadf8627b0433c62dfef472c051f6d377b571
MafaldaIsidoro <>
2023-03-27 22:03:49 +0100

    javascript
    javascript

--- exercicio6/scripts.js
+++ exercicio6/scripts.js
@@ -1,3 +1,19 @@
 function mostraConteudoMenu(){
-    document.getElementById("itens-mobile").style.display="block";
+
+    if( document.getElementById("itens-mobile").style.display=="none")
+    {
+        document.getElementById("texto-menu").innerHTML='<i class="fa-solid fa-xmark icon-menu-principal"></i>';
+        document.getElementById("itens-mobile").style.display="block";
+    } else{
+        document.getElementById("texto-menu").innerHTML='<i class="fa-sharp fa-solid fa-bars icon-menu-principal"></i>';
+        document.getElementById("itens-mobile").style.display="none";
+    }
+}
+
+function normalizaMenuMobile(){
+    largura = window.innerWidth;
+    if (largura > 850){
+    document.getElementById("texto-menu").innerHTML='<i class="fa-sharp fa-solid fa-bars icon-menu-principal"></i>';
+    document.getElementById("itens-mobile").style.display="none";
+    }
 }
