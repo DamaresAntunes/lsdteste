75240e5e70351cc008aed6d292871e5686404362
MafaldaIsidoro <>
2023-03-27 22:18:03 +0100

    jvs
    jvs

---
+++ javascript7/scripts2.js
@@ -0,0 +1,10 @@
+document.querySelector('input').addEventListener('focus', mostraConteudoInput);
+document.querySelector('input').addEventListener('blur', escondeConteudoInput)
+
+function mostraConteudoInput(){
+    alert('entrei ')
+}
+
+function mostraConteudoInput(){
+    alert('entrei  para esconder')
+}
